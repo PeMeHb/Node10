5565ba6a084eeec007df952b3d67a50911fe1d1a
PeMeHb <pemehb@i.ua>
2017-11-04 11:33:40 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,7 +3,4 @@ const header = require("./moduls/header/header")();
 const main = require("./moduls/main/main")();
 const footer = require("./moduls/footer/footer")();
 
-
-document.body.appendChild(header);
-document.body.appendChild(main);
-document.body.appendChild(footer);
+$('body').append(header).append(main).append(footer);
